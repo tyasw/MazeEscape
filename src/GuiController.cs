fde19139fdbfab7d7fc19b9d9d9e38af9bfc87c9
William <tyasw@wwu.edu>
2018-12-27 16:56:56 -0800

    Move code back to one repo

---
+++ src/GuiController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/** GuiController.cs
+ * 
+ * Controls the creation of the maze, and the view presented to the user for the
+ * GUI version of the game.
+ */
+namespace MazeEscapeLibrary.src
+{
+    public class GuiController : GameController
+    {
+        public GameModel GameModel { get; set; }
+        public GameView GameView { get; set; }
+        //public CommandParser CmdParser { get; set; }
+
+        public GuiController()
+        {
+            GameModel = new GameModel();
+            GameView = new GuiView(GameModel);
+            //CmdParser = new CommandParser();
+        }
+
+        public void Start()
+        {
+            AddCommand(new ShowNewGameOptionsCommand(this));
+            AddCommand(new BeginGameCommand(this, GameModel));
+
+            while (true)
+            {
+                RunNextCommand();
+            }
+
+            //...
+        }
+
+        public void ShowMainMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCommand(Command command)
+        {
+            //CmdParser.AddCommand(command);
+        }
+
+        public void ShowGameOptions()
+        {
+            try
+            {
+                GetMazeWidthAndHeightFromUser();
+            } catch (Exception ex)
+            {
+                LogError(ex);
+            }
+        }
+
+        private void LogError(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        private void GetMazeWidthAndHeightFromUser()
+        {
+            Console.WriteLine("What size of maze do you want?");
+            Console.Write("Width: ");
+            int width = Int32.Parse(Console.ReadLine().Trim());
+            Console.Write("Height: ");
+            int height = Int32.Parse(Console.ReadLine().Trim());
+
+            GameModel.SetMazeWidth(width);
+            GameModel.SetMazeHeight(height);
+        }
+
+        public void FireDrawMaze()
+        {
+            GameView.ShowMaze();
+        }
+
+        public void FireDrawWorld()
+        {
+            //...
+            FireDrawMaze();
+        }
+
+        public void RunNextCommand()
+        {
+            //CmdParser.RunNextCommand();
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/** MazeTest.cs
+ * 
+ * Author: William Tyas
+ * Description: MazeTest is a game where users find their way through a maze.
+ */
+namespace MazeEscapeLibrary.src
+{
+    public class Program
+    {
+        public static int MazeWidth { get; set; }
+        public static int MazeHeight { get; set; }
+
+        static void Main(string[] args)
+        {
+            GameController gameController = new TextController();
+            gameController.Start();
+        }
+    }
+}
