6b8e3524861c13fb1e652417efc8d31dceea2462
William <wtyas@hotmail.com>
2019-05-16 19:40:05 -0700

    Create FinishGameCommand

---
+++ Assets/Scripts/Commands/FinishGameCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Commands {
+    class FinishGameCommand : Command {
+        private void Awake() {
+            ClassFactory classFactory = ClassFactory.GetInstance();
+            Subjects = new List<Subject>();
+            Subject Event = classFactory.GetWonGameEvent();
+            Subjects.Add(Event);
+        }
+
+        public override void Run() {
+            base.Run();
+        }
+
+        public override string ToString() {
+            return "WonGameCommand";
+        }
+    }
+}
