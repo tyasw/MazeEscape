3b77a695df6fd8cdaf69b50d3f48376dbc3f42ea
William <wtyas@hotmail.com>
2019-06-01 09:35:08 -0700

    Add boundary area around entrance to maze
    The player should not be able to cheat by walking around the outside
    of the maze. An area they must stay within was added.

--- Assets/Scripts/UnityView.cs
+++ Assets/Scripts/UnityView.cs
@@ -24,12 +24,73 @@ public class UnityView : MonoBehaviour, GameView {
 
     public void DrawMaze() {
         DrawMazeWalls();
+        DrawBoundaryAreas();
         CreateStartAndEndTriggers();
     }
 
     private void DrawMazeWalls() {
         MazeDrawer mazeDrawer = new MazeDrawer(MazeData);
         mazeDrawer.DrawMaze();
+    }
+
+    private void DrawBoundaryAreas() {
+        DrawEntryBoundaryArea();
+        DrawExitBoundaryArea();
+    }
+
+    private void DrawEntryBoundaryArea() {
+        GameObject wallOne = CreateBoundaryWall(1.0f, MazeData.CellSize, MazeData.CellSize * 2);
+        wallOne.name = "EntryBoundaryWallOne";
+        float wallOneXPos = wallOne.transform.position.x - 0.5f;
+        float wallOneYPos = wallOne.transform.position.y;
+        float wallOneZPos = wallOne.transform.position.z - 5.05f;
+        wallOne.transform.position = new Vector3(wallOneXPos, wallOneYPos, wallOneZPos);
+        GameObject wallTwo = CreateBoundaryWall(1.0f, MazeData.CellSize, MazeData.CellSize * 2);
+        wallTwo.name = "EntryBoundaryWallTwo";
+        float wallTwoXPos = wallTwo.transform.position.x + 10.6f;
+        float wallTwoYPos = wallTwo.transform.position.y;
+        float wallTwoZPos = wallTwo.transform.position.z - 5.05f;
+        wallTwo.transform.position = new Vector3(wallTwoXPos, wallTwoYPos, wallTwoZPos);
+        GameObject wallThree = CreateBoundaryWall(MazeData.CellSize + 2.2f, MazeData.CellSize, 1.0f);
+        wallThree.name = "EntryBoundaryWallThree";
+        float wallThreeXPos = wallThree.transform.position.x + 5.1f;
+        float wallThreeYPos = wallThree.transform.position.y;
+        float wallThreeZPos = wallThree.transform.position.z - 15.55f;
+        wallThree.transform.position = new Vector3(wallThreeXPos, wallThreeYPos, wallThreeZPos);
+    }
+
+    private void DrawExitBoundaryArea() {
+
+    }
+
+    private GameObject CreateBoundaryWall(float xScale, float yScale, float zScale) {
+        float WallWidth = MazeData.CellSize;
+        float WallThickness = MazeData.CellWallThickness;
+
+        float totalWidth = WallWidth + 2 * WallThickness;
+        float cellHeight = WallWidth; // Change later?
+
+        float xPos = 0.0f;
+        float yPos = cellHeight / 2;
+        float zPos = -1 * (WallWidth + WallThickness) + (cellHeight + WallThickness) / 2;
+
+        GameObject entryBoundaryArea = ObjectFactory.CreateGameObject("EntryBoundaryArea");
+        entryBoundaryArea.transform.position = new Vector3(xPos, yPos, zPos);
+        entryBoundaryArea.transform.localScale = new Vector3(xScale, yScale, zScale);
+        entryBoundaryArea = AddColliderToBoundaryWall(entryBoundaryArea);
+        return entryBoundaryArea;
+    }
+
+    private GameObject AddColliderToBoundaryWall(GameObject wall) {
+        wall.AddComponent<BoxCollider>();
+        float xScale = wall.transform.localScale.x + 0.1f;
+        float yScale = wall.transform.localScale.y;
+        float zScale = wall.transform.localScale.z + 0.1f;
+        Collider collider = wall.GetComponent<BoxCollider>();
+        collider.isTrigger = false;
+        collider.transform.localScale = new Vector3(xScale, yScale, zScale);
+
+        return wall;
     }
 
     private void CreateStartAndEndTriggers() {
