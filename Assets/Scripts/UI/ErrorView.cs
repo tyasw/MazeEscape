1c351a603e90e1a84d0ea1ab1bcb3b0053139bec
William <tyasw@wwu.edu>
2019-09-17 18:54:44 -0700

    Create area for displaying error messages

---
+++ Assets/Scripts/UI/ErrorView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI {
+    public class ErrorView : MonoBehaviour {
+        private List<string> errors = new List<string>();
+
+        private void Start() {
+            foreach (string error in errors) {
+                GameObject errorObject = new GameObject("Error");
+                errorObject.AddComponent<Text>();
+                Text text = errorObject.GetComponent<Text>();
+                text.text = error;
+                text.font = Font.CreateDynamicFontFromOSFont("Arial", 14);
+                errorObject.transform.SetParent(this.gameObject.transform);
+            }
+        }
+    }
+}
